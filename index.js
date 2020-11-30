2e93fe8f75fe22e7e0874fb065dc5c728aade179
ZenekeZene <hectorvillarm@gmail.com>
2020-12-01 00:18:13 +0100

    4.- Read it

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-process.stdin.pipe(process.stdout)
+const { Readable } = require('stream')
+
+const myStream = new Readable({})
+myStream._read = () => {}
+myStream.push(process.argv[2])
+myStream.pipe(process.stdout)
+
