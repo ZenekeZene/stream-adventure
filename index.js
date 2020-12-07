f4415de6405f2f509617a5525c91775fa133c426
ZenekeZene <hectorvillarm@gmail.com>
2020-12-07 01:03:54 +0100

    Step-8

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-const { doesNotMatch } = require('assert')
-const split2 = require('split2')
-const through2 = require('through2')
+const concat = require('concat-stream')
 
-const isEven = (number) => number % 2 === 0
-
-let lineCount = 0
-
-const stream = through2(
-  function (chunk, encoding, next) {
-    let line = chunk.toString()
-    line = (isEven(lineCount))
-      ? line.toLowerCase()
-      : line.toUpperCase()
-    this.push(line + '\n')
-    lineCount++;
-    next()
-  }
-)
-
-process.stdin
-  .pipe(split2())
-  .pipe(stream)
-  .pipe(process.stdout)
+process.stdin.pipe(concat(function (body) {
+  process.stdout.write(body.toString().split('').reverse().join(''))
+}))
